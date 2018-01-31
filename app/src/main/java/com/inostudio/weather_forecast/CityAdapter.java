e562b1702ffd8790e6e233d4f4f5afb0df5040c9
Ivan <Ivan@example.com>
2018-01-31 13:04:39 +0300

    include database

---
+++ app/src/main/java/com/inostudio/weather_forecast/CityAdapter.java
@@ -0,0 +1,8 @@
+package com.inostudio.weather_forecast;
+
+/**
+ * Created by Иван on 30.01.2018.
+ */
+
+class CityAdapter {
+}
---
+++ app/src/main/java/com/inostudio/weather_forecast/database/AppDatabase.java
@@ -0,0 +1,8 @@
+package com.inostudio.weather_forecast.database;
+
+/**
+ * Created by Иван on 30.01.2018.
+ */
+
+public class AppDatabase {
+}
