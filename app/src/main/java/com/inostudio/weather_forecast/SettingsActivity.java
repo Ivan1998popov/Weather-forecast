9715fa3403026cddbbf91e7e0e6f08fb5823e905
Anatoly <Anatoly-Vinnikov>
2018-02-05 19:51:24 +0300

    DataBase 2.0
    Weather now completely synchronized with server and DB updating
    correctly
    Added APK to quick install

--- app/src/main/java/com/inostudio/weather_forecast/SettingsActivity.java
+++ app/src/main/java/com/inostudio/weather_forecast/SettingsActivity.java
@@ -5,13 +5,11 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.inostudio.weather_forecast.database.AppDatabase;
-import com.inostudio.weather_forecast.database.City;
 import com.inostudio.weather_forecast.database.Temperature;
 
 import org.json.JSONArray;
@@ -21,24 +19,18 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.List;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 
 public class SettingsActivity extends AppCompatActivity {
 
     public Button btnUpdateDatabase;
-    public List<City> cities;
-    public List<Temperature> temperatures, temperaturesToAdd;
+    public List<Temperature> temperaturesToAdd;
     public Context ctx = this;
 
-    public CyclicBarrier gate = new CyclicBarrier(11);
-
-    String temperatureStr, wind_speedStr, pressureStr, humidityStr/*, weather_typeStr, weather_descrStr*/;
+    String temperatureStr, wind_speedStr, pressureStr, humidityStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,58 +39,45 @@ public class SettingsActivity extends AppCompatActivity {
         final AppDatabase db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "weather-forecast")
                 .allowMainThreadQueries().build();
 
-        cities = db.mCityDao().getAllCities();
-        temperatures = db.mTemperatureDao().getAllTemperature();
         btnUpdateDatabase = findViewById(R.id.update_database);
         btnUpdateDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 temperaturesToAdd = db.mTemperatureDao().getAllTemperature();
-                temperaturesToAdd.clear();
-                String[] list_cities =getResources().getStringArray(R.array.update_cities);
-                for (int i = 0; i <list_cities.length; i++) {
-                    new OpenWeatherMapTask( list_cities[i]).execute();
-               }
-
-                try {
-                    gate.await();
-                } catch (InterruptedException | BrokenBarrierException e) {
-                    e.printStackTrace();
+                String[] list_cities = getResources().getStringArray(R.array.update_cities);
+                for (int i = 0; i < list_cities.length; i++) {
+                    new OpenWeatherMapTask(list_cities[i], i, db).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                 }
-
-                for (int i = 1; i <= temperaturesToAdd.size(); i++) {
-                    City city = db.mCityDao().getCity(list_cities[i]);
-                    Temperature temp = db.mTemperatureDao().getWeatherById(city.getId());
-                    temp.setTemperature(temperaturesToAdd.get(city.getId()).getTemperature());
-                    //db.mTemperatureDao().insertAll(temperaturesToAdd.get(i));
-                    //    temp.setPressure(temperaturesToAdd.get(i).getPressure());
-                    //   temp.setHumidity(temperaturesToAdd.get(i).getHumidity());
-                    db.mTemperatureDao().updateTemperature(temp);
-
-                }
-//                Temperature temp = db.mTemperatureDao().getWeatherById(1);
-//                temp.setTemperature(String.valueOf(500));
-//                db.mTemperatureDao().updateTemperature(temp);
-
-                Toast.makeText(ctx, R.string.file_saved, Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    public void updateDB(AppDatabase db) {
+        for (int i = 0; i < temperaturesToAdd.size(); i++) {
+            db.mTemperatureDao().updateTemperature(temperaturesToAdd.get(i));
+        }
+
+        Toast.makeText(ctx, R.string.db_updated, Toast.LENGTH_SHORT).show();
+    }
+
     public String trans_kel(String kel) {
-        double cel = 273 - Double.parseDouble(kel);
+        double cel = Double.parseDouble(kel) - 273.15;
         return String.valueOf(Math.rint(cel));
     }
 
     private class OpenWeatherMapTask extends AsyncTask<Void, Void, String> {
 
         String cityName;
-        String cel;
         String dummyAppid = "293da20ad6da8e2bb2974cc9760fbf87";
         String queryWeather = "http://api.openweathermap.org/data/2.5/weather?q=";
         String queryDummyKey = "&appid=" + dummyAppid;
+        Integer cityID;
+        AppDatabase db;
 
-        OpenWeatherMapTask( String cityName) {
+        OpenWeatherMapTask(String cityName, Integer cityID, AppDatabase db) {
             this.cityName = cityName;
+            this.cityID = cityID;
+            this.db = db;
         }
 
         @Override
@@ -113,31 +92,20 @@ public class SettingsActivity extends AppCompatActivity {
                 result += ParseJSON(queryReturn);
             } catch (IOException e) {
                 e.printStackTrace();
-                queryReturn = e.getMessage();
             }
-
-            final String finalQueryReturn = query + "\n\n" + queryReturn;
-//            runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    //textViewInfo.setText(finalQueryReturn);
-//
-//                }
-//            });
 
             return result;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            Temperature temperature = new Temperature(temperatureStr, wind_speedStr,
-                    pressureStr, humidityStr);
-            temperaturesToAdd.add(temperature);
-//            try {
-//                gate.await();
-//            } catch (InterruptedException | BrokenBarrierException e) {
-//                e.printStackTrace();
-//            }
+            temperaturesToAdd.get(cityID).setTemperature(temperatureStr);
+            temperaturesToAdd.get(cityID).setWindSpeed(wind_speedStr);
+            temperaturesToAdd.get(cityID).setPressure(pressureStr);
+            temperaturesToAdd.get(cityID).setHumidity(humidityStr);
+            if (cityID == 9) {
+                updateDB(db);
+            }
         }
 
         private String sendQuery(String query) throws IOException {
@@ -148,7 +116,7 @@ public class SettingsActivity extends AppCompatActivity {
             HttpURLConnection httpURLConnection = (HttpURLConnection) searchURL.openConnection();
             if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 InputStreamReader inputStreamReader = new InputStreamReader(httpURLConnection.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader,8192);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader, 8192);
 
                 String line = null;
                 while ((line = bufferedReader.readLine()) != null) {
@@ -202,7 +170,7 @@ public class SettingsActivity extends AppCompatActivity {
                         JSONObject main = jsonHelperGetJSONObject(JsonObject, "main");
                         if (main != null) {
                             jsonResult += "temp: " + jsonHelperGetString(main, "temp") + "\n";   //здесь можно все данные вытащить
-                            temperatureStr = jsonHelperGetString(main, "temp");
+                            temperatureStr = trans_kel(jsonHelperGetString(main, "temp"));
                             jsonResult += "pressure: " + jsonHelperGetString(main, "pressure") + "\n";
                             pressureStr = jsonHelperGetString(main, "pressure");
                             jsonResult += "humidity: " + jsonHelperGetString(main, "humidity") + "\n";
